d8791baa09ce1f16fd6848b68d7334c42b219ef1
syedsajjadaskari <syedsajjad62@gmail.com>
2023-01-04 10:32:46 +0530

    add logger

--- src/deepClassifier/pipeline/stage_01_Data_ingestion.py
+++ src/deepClassifier/pipeline/stage_01_Data_ingestion.py
@@ -1,6 +1,6 @@
 from deepClassifier.config import ConfigurationManager
 from deepClassifier.components import DataIngestion
-
+from deepClassifier import logger
 STAGE_NAME = "Data Ingestion stage"
 
 from deepClassifier import logger
@@ -15,11 +15,12 @@ def main():
 
 
 
+
 if __name__ == '__main__':
     try:
-        logger.info(f">>>> stage {STAGE_NAME} started <<<<<")
+        logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
         main()
-        logger.info(f">>>> stage {STAGE_NAME} completed <<<<")
+        logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
     except Exception as e:
         logger.exception(e)
         raise e
