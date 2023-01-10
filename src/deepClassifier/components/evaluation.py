bbea71e0394f5e74962bfc38997dfc8a0b10e96c
syedsajjadaskari <syedsajjad62@gmail.com>
2023-01-10 09:18:33 +0530

    Add Evaluation

--- src/deepClassifier/components/evaluation.py
+++ src/deepClassifier/components/evaluation.py
@@ -43,3 +43,7 @@ class Evaluation:
         model = self.load_model(self.config.path_of_model)
         self._valid_generator()
         self.score = model.evaluate(self.valid_generator)
+
+    def save_score(self):
+        scores = {"loss": self.score[0], "accuracy": self.score[1]}
+        save_json(path=Path("scores.json"), data=scores)
